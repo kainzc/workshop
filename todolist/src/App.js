44c687a9d49af61340fd79e946aade5ca6394acf
kainzc <colek1005@gmail.com>
2022-03-01 19:42:25 -0600

    myfirstjscode

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,8 +4,30 @@ import './App.css';
 function App() {
   return (
     <div style="color:">
-      <h1>Template header</h1>
-      <p>Hello world</p>
+      
+        <div class="red-headings">
+        
+        <h1 style="color: blue;">My first website!</h1>
+        <h3>My first website! but smaller</h3>
+        <p>Check this out. our first paragraph</p>
+
+        </div>
+
+
+        <div class ="small-green-text">
+            <h5>First Green Text</h5>
+            <h2>Second Green Text</h2>
+        </div>
+
+        <p>Paragraph 2</p>
+        <p>Paragraph 3</p>
+        <img src="https://images.newrepublic.com/b53d30e509b44bfc5f7bf3d3a03cc000fcd50623.jpeg?auto=compress&w=768&h=undefined&ar=3%3A2&fit=crop&crop=faces&q=65&fm=jpg&ixlib=react-9.0.2"/>
+        <br /><br /><br />
+        
+
+        <a href="https://google.com">Go to Google</a>
+        <p>Paragraph 4</p>
+        <marquee>moving text</marquee>
     </div>
   );
 }
