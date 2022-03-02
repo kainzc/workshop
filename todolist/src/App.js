62e7b8b27bdf29e30c5df6faca1f96f4db7786a7
kainzc <colek1005@gmail.com>
2022-03-01 19:36:04 -0600

    message

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style="color:">
+      <h1>Template header</h1>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
